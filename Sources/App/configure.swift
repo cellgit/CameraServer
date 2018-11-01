25b37a4b2482060bff803f50999cb20e5796b0e9
kokora <liuhongli2120@gmail.com>
2018-11-02 01:59:53 +0800

    22222

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -27,16 +27,44 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     
     let databaseConfig:PostgreSQLDatabaseConfig
     
+//    if let url = Environment.get("DATABASE_URL") {
+////        databaseConfig = try PostgreSQLDatabaseConfig(url: url)
+//        databaseConfig = PostgreSQLDatabaseConfig(url: url)!
+//    }
+//    else {
+//        databaseConfig = PostgreSQLDatabaseConfig(hostname: "localhost",
+//                                                  port: 5432,
+//                                                  username: "postgres",
+//                                                  database: "kokora")
+//    }
+    
     if let url = Environment.get("DATABASE_URL") {
-//        databaseConfig = try PostgreSQLDatabaseConfig(url: url)
-        databaseConfig = PostgreSQLDatabaseConfig(url: url)!
+        guard let urlConfig = PostgreSQLDatabaseConfig(url: url) else {
+            fatalError("Failed to create PostgresConfig")
+        }
+        databaseConfig = urlConfig
+    } else {
+        let databaseName: String
+        let databasePort: Int
+        if (env == .testing) {
+            databaseName = "vapor-test"
+            if let testPort = Environment.get("DATABASE_PORT") {
+                databasePort = Int(testPort) ?? 5433
+            } else {
+                databasePort = 5433
+            }
+        }
+        else {
+            databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+            databasePort = 5432
+        }
+        let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+        let username = Environment.get("DATABASE_USER") ?? "vapor"
+        let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+        databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: databasePort, username: username, database: databaseName, password: password)
     }
-    else {
-        databaseConfig = PostgreSQLDatabaseConfig(hostname: "localhost",
-                                                  port: 5432,
-                                                  username: "postgres",
-                                                  database: "kokora")
-    }
+    
+    
     
     let database = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .psql)
