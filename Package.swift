9367a0bd50e81afc0889095b574c310a8c25baae
kokora <liuhongli2120@gmail.com>
2018-11-08 00:09:03 +0800

    * add dependences

--- Package.swift
+++ Package.swift
@@ -13,13 +13,46 @@ let package = Package(
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+        
+        
+        
+        
+        ///
+        .package(url: "https://github.com/skelpo/APIErrorMiddleware.git", from: "0.1.0"),
+        .package(url: "https://github.com/IBM-Swift/Swift-SMTP.git", from: "4.0.1"),
+        
+        // JWT Middleware to authenticate
+        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/skelpo/JWTMiddleware.git", from: "0.6.1"),
+        
+        .package(url: "https://github.com/vapor/multipart.git", from: "3.0.0"),
+        
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
+        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/console.git", from: "3.0.0"),
+        
+        .package(url: "https://github.com/vapor/redis.git", from: "3.0.0-rc"),
+        
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.1"),
+        .package(url: "https://github.com/PerfectSideRepos/Perfect-ICONV.git",from:"3.0.1")
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentPostgreSQL",
                                             "FluentSQLite",
                                             "Leaf",
-                                            "Vapor"]),
+                                            "Vapor",
+                                            
+                                            "PerfectICONV",
+                                            "JWTMiddleware",
+                                            "JWT",
+                                            "Multipart",
+                                            "Authentication",
+                                            "Crypto",
+                                            "Logging",
+                                            "Redis",
+                                            "SwiftSoup",
+                                            "APIErrorMiddleware"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
